80688cc5675881067a50c7ef3a8d04c959595941
Alexej Disterhoft <alexej@disterhoft.de>
2024-01-09 22:25:23 +0100

    refactor: remove duplicate util definitions
    Signed-off-by: Alexej Disterhoft <alexej@disterhoft.de>

--- internal/util/cli.go
+++ internal/util/cli.go
@@ -3,55 +3,25 @@ package util
 import (
 	"fmt"
 	"strings"
-	"time"
-
-	"github.com/xhit/go-str2duration/v2"
 )
 
 const (
 	indend = `  `
 )
 
-// DurationValue is a wrapper around time.Duration that implements the
-// pflag.Value interface.
-//
-// Reason: the default time.Duration implementation of pflag.Value only supports
-// units up to hours. That's not really enough for us as we most likely want to
-// check for tickets that expire in a few days or even weeks. This wrapper uses
-// the go-str2duration library to additionally support days and weeks.
-type DurationValue time.Duration
-
-// NewDurationValue returns a new DurationValue with the specified time.Duration
-func NewDurationValue(val time.Duration) *DurationValue {
-	return (*DurationValue)(&val)
-}
-
-// Set implements the pflag.Value interface for DurationValue
-func (d *DurationValue) Set(s string) error {
-	v, err := str2duration.ParseDuration(s)
-	*d = DurationValue(v)
-	return err
-}
-
-// Type implements the pflag.Value interface for DurationValue
-func (d *DurationValue) Type() string {
-	return "duration"
-}
-
-// String implements the pflag.Value interface for DurationValue
-func (d *DurationValue) String() string {
-	return time.Duration(*d).String()
-}
-
-// Cast returns the underlying time.Duration value
-func (d *DurationValue) Cast() time.Duration {
-	return time.Duration(*d)
-}
-
 type stringNormalizer struct {
 	string
 }
 
+// StringSliceToFlagOptions returns a normalized string representation of a string slice
+// separated by commas and spaces, suitable for use in CLI flag usage strings, e.g.
+// "one, two, three".
+func StringSliceToFlagOptions(slice []string) string {
+	return strings.Join(slice, ", ")
+}
+
+// CliShortDesc returns a normalized long description of a CLI command, the desc string
+// is passed through fmt.Sprintf() with the args as arguments.
 func CliLongDesc(desc string, args ...any) string {
 	if desc == "" || len(desc) == 0 {
 		return ""
@@ -62,6 +32,8 @@ func CliLongDesc(desc string, args ...any) string {
 	return stringNormalizer{desc}.trim().string
 }
 
+// CliExample returns a normalized example of a CLI command, the example string
+// is passed through fmt.Sprintf() with the args as arguments.
 func CliExample(example string, args ...any) string {
 	if example == "" || len(example) == 0 {
 		return ""
@@ -89,13 +61,13 @@ func (s stringNormalizer) indent() stringNormalizer {
 	lines := strings.Split(s.string, "\n")
 
 	for i, line := range lines {
-		lines[i] = indend + line
+		if line != "" {
+			lines[i] = indend + line
+		} else {
+			lines[i] = ""
+		}
 	}
 
 	s.string = strings.Join(lines, "\n")
 	return s
-}
-
-func StringSliceToFlagOptions(slice []string) string {
-	return strings.Join(slice, ", ")
 }
