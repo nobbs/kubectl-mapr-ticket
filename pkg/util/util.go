ddd57a6204739b577de058cb2492b108e293c0f6
Alexej Disterhoft <alexej@disterhoft.de>
2024-01-27 21:52:40 +0100

    chore: add license header to all files

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2024 Alexej Disterhoft
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+//
+// SPX-License-Identifier: MIT
+
 package util
 
 import "strings"
