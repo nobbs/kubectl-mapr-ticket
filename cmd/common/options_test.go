0ba877609b34707b567cbb6a93559b2b42e062c9
Alexej Disterhoft <alexej@disterhoft.de>
2024-02-16 22:10:30 +0100

    test: add more tests for various code paths

--- cmd/common/options_test.go
+++ cmd/common/options_test.go
@@ -4,3 +4,28 @@
 // SPDX-License-Identifier: MIT
 
 package common_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	. "github.com/nobbs/kubectl-mapr-ticket/cmd/common"
+
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/cli-runtime/pkg/genericiooptions"
+)
+
+// nolint:paralleltest
+func TestNewOptions(t *testing.T) {
+	// Create a new ConfigFlags and IOStreams
+	flags := &genericclioptions.ConfigFlags{}
+	streams := genericiooptions.IOStreams{}
+
+	// Call the NewOptions function
+	options := NewOptions(flags, streams)
+
+	// Verify that the returned options match the input values
+	assert.Equal(t, flags, options.KubernetesConfigFlags)
+	assert.Equal(t, streams, options.IOStreams)
+}
